dedca87e5fcfd5abab4b467c70ac3604f85f8c9e
harshit-3004 <h.chaubisa30@gmail.com>
2022-03-26 15:13:01 +0530

    correcting the first question

--- question-1.js
+++ question-1.js
@@ -1,14 +1,17 @@
-let arr = [1,5,2,6,3,7,4,8];
+let arr = [1,0,5,2,6,3,7,4,8];
 let leng = arr.length;
 
 let even = 0;
 let odd = 0;
+let zeros = 0;
 for(let i=0; i<leng;i++){
-    if(arr[i]%2==0)
+    if(arr[i]==0)
+        zeros++;
+    else if(arr[i]%2==0)
         odd++;
-    
     else
         even++;
 }
 console.log(odd);
 console.log(even);
+console.log(zeros);
